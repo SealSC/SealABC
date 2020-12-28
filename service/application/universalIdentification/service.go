f14460266c11eb076a70b63c3b1626eda4092520
AKACoder <akacoder@outlook.com>
2020-12-28 14:53:57 +0800

    add universal identification application framework

---
+++ service/application/universalIdentification/service.go
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020 The SealABC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package universalIdentification
+
+import (
+	"github.com/SealSC/SealABC/log"
+	"github.com/SealSC/SealABC/service/application/universalIdentification/uidInterface"
+	"github.com/SealSC/SealABC/service/system/blockchain/chainStructure"
+	"github.com/SealSC/SealABC/storage/db"
+)
+
+func Load() {
+
+}
+
+func NewBasicAssetsApplication(config Config) (app chainStructure.IBlockchainExternalApplication, err error) {
+	kvDriver, err := db.NewKVDatabaseDriver(config.KVDBName, config.KVDBConfig)
+	if err != nil {
+		log.Log.Error("can't load basic assets app for now: ", err.Error())
+		return
+	}
+
+	sqlStorage := config.SQLStorage
+	if !config.EnableSQLDB {
+		sqlStorage = nil
+	}
+	app = uidInterface.NewApplicationInterface(kvDriver, sqlStorage)
+
+	return
+}
+
---
+++ service/application/universalIdentification/uidInterface/uidInterface.go
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2020 The SealABC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package uidInterface
+
+import (
+	"github.com/SealSC/SealABC/dataStructure/enum"
+	"github.com/SealSC/SealABC/metadata/applicationResult"
+	"github.com/SealSC/SealABC/metadata/block"
+	"github.com/SealSC/SealABC/metadata/blockchainRequest"
+	"github.com/SealSC/SealABC/service"
+	"github.com/SealSC/SealABC/service/application/basicAssets/basicAssetsLedger"
+	"github.com/SealSC/SealABC/service/application/basicAssets/basicAssetsSQLStorage"
+	"github.com/SealSC/SealABC/service/system/blockchain/chainStructure"
+	"github.com/SealSC/SealABC/storage/db/dbInterface/kvDatabase"
+	"github.com/SealSC/SealABC/storage/db/dbInterface/simpleSQLDatabase"
+)
+
+var QueryDBType struct{
+	KV  enum.Element
+	SQL enum.Element
+}
+
+type UniversalIdentification struct {
+
+}
+
+func (u *UniversalIdentification) Name() (name string) {
+	return "Universal Identification"
+}
+
+func (u *UniversalIdentification) PushClientRequest(req blockchainRequest.Entity) (result interface{}, err error) {
+	return
+}
+
+func (u *UniversalIdentification) Query(req []byte) (result interface{}, err error) {
+	return
+}
+
+func (u *UniversalIdentification) PreExecute(req blockchainRequest.Entity, _ block.Entity) (result []byte, err error) {
+	return
+}
+
+func (u *UniversalIdentification) Execute(
+	req blockchainRequest.Entity,
+	blk block.Entity,
+	actIndex uint32,
+) (result applicationResult.Entity, err error) {return}
+
+func (u *UniversalIdentification) Cancel(req blockchainRequest.Entity) (err error) {return}
+
+func (u *UniversalIdentification) RequestsForBlock(_ block.Entity) (reqList []blockchainRequest.Entity, cnt uint32) {return}
+
+func (u *UniversalIdentification) Information() (info service.BasicInformation) {
+	info.Name = u.Name()
+	info.Description = "this is a universal identification application"
+
+	info.Api.Protocol = service.ApiProtocols.INTERNAL.String()
+	info.Api.Address = ""
+	info.Api.ApiList = []service.ApiInterface {}
+	return
+}
+
+func (u *UniversalIdentification) SetBlockchainService(_ interface{}) {}
+func (u *UniversalIdentification) UnpackingActionsAsRequests(_ blockchainRequest.Entity) ([]blockchainRequest.Entity, error) {return nil, nil}
+func (u *UniversalIdentification) GetActionAsRequest(req blockchainRequest.Entity) (newReq blockchainRequest.Entity) {
+	return
+}
+
+func Load()  {
+	enum.SimpleBuild(&QueryDBType)
+	basicAssetsLedger.Load()
+	basicAssetsSQLStorage.Load()
+}
+
+func NewApplicationInterface(kvDriver kvDatabase.IDriver, sqlDriver simpleSQLDatabase.IDriver) (app chainStructure.IBlockchainExternalApplication) {
+	return &UniversalIdentification{}
+}
