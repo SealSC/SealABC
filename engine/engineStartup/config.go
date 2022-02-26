6a7d49658de3d4594b6549907c7c7e16b6230c60
AKACoder <akacoder@outlook.com>
2022-02-26 19:43:23 +0800

    remove storage config in engine confg

--- engine/engineStartup/config.go
+++ engine/engineStartup/config.go
@@ -27,7 +27,6 @@ type Config struct {
     Api         engineApi.Config
     Log         log.Config
 
-    StorageConfig           interface{}
     ConsensusNetwork        network.Config
     ConsensusDisabled       bool
 
