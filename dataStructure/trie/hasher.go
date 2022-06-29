655c16f4d8daa44b90f0a279c7d7e3b9a32136bd
bianning <bianning18516822189@gmail.com>
2022-06-29 17:05:20 +0800

    feat: add MPT

---
+++ dataStructure/trie/hasher.go
@@ -0,0 +1,161 @@
+package trie
+
+import (
+	"bytes"
+	"github.com/SealSC/SealABC/common"
+	"github.com/SealSC/SealABC/crypto/hashes/sha3"
+	"github.com/SealSC/SealABC/storage/db/dbInterface/kvDatabase"
+	"github.com/ethereum/go-ethereum/rlp"
+	"hash"
+
+	"sync"
+)
+
+type hasher struct {
+	tmp                  *bytes.Buffer
+	sha                  hash.Hash
+	cacheGen, cacheLimit uint16
+}
+
+var hasherPool = sync.Pool{
+	New: func() interface{} {
+		//todo: hash
+		return &hasher{tmp: new(bytes.Buffer), sha: sha3.Keccak256.OriginalHash()()}
+	},
+}
+
+func newHasher(cacheGen, cacheLimit uint16) *hasher {
+	h := hasherPool.Get().(*hasher)
+	h.cacheGen, h.cacheLimit = cacheGen, cacheLimit
+	return h
+}
+
+func returnHasherToPool(h *hasher) {
+	hasherPool.Put(h)
+}
+
+// hash collapses a node down into a hash node, also returning a copy of the
+// original node initialized with the computed hash to replace the original one.
+func (h *hasher) hash(n node, db kvDatabase.IDriver, force bool) (node, node, error) {
+	// If we're not storing the node, just hashing, use available cached data
+	if hash, dirty := n.cache(); hash != nil {
+		if db == nil {
+			return hash, n, nil
+		}
+		if n.canUnload(h.cacheGen, h.cacheLimit) {
+			// Unload the node from cache. All of its subnodes will have a lower or equal
+			// cache generation number.
+			cacheUnloadCounter.Inc(1)
+			return hash, hash, nil
+		}
+		if !dirty {
+			return hash, n, nil
+		}
+	}
+	// Trie not processed yet or needs storage, walk the children
+	collapsed, cached, err := h.hashChildren(n, db)
+	if err != nil {
+		return hashNode{}, n, err
+	}
+	hashed, err := h.store(collapsed, db, force)
+	if err != nil {
+		return hashNode{}, n, err
+	}
+	// Cache the hash of the node for later reuse and remove
+	// the dirty flag in commit mode. It's fine to assign these values directly
+	// without copying the node first because hashChildren copies it.
+	cachedHash, _ := hashed.(hashNode)
+	switch cn := cached.(type) {
+	case *shortNode:
+		cn.flags.hash = cachedHash
+		if db != nil {
+			cn.flags.dirty = false
+		}
+	case *fullNode:
+		cn.flags.hash = cachedHash
+		if db != nil {
+			cn.flags.dirty = false
+		}
+	}
+	return hashed, cached, nil
+}
+
+// hashChildren replaces the children of a node with their hashes if the encoded
+// size of the child is larger than a hash, returning the collapsed node as well
+// as a replacement for the original node with the child hashes cached in.
+func (h *hasher) hashChildren(original node, db kvDatabase.IDriver) (node, node, error) {
+	var err error
+
+	switch n := original.(type) {
+	case *shortNode:
+		// Hash the short node's child, caching the newly hashed subtree
+		collapsed, cached := n.copy(), n.copy()
+		collapsed.Key = hexToCompact(n.Key)
+		cached.Key = common.CopyBytes(n.Key)
+
+		if _, ok := n.Val.(valueNode); !ok {
+			collapsed.Val, cached.Val, err = h.hash(n.Val, db, false)
+			if err != nil {
+				return original, original, err
+			}
+		}
+		if collapsed.Val == nil {
+			collapsed.Val = valueNode(nil) // Ensure that nil children are encoded as empty strings.
+		}
+		return collapsed, cached, nil
+
+	case *fullNode:
+		// Hash the full node's children, caching the newly hashed subtrees
+		collapsed, cached := n.copy(), n.copy()
+
+		for i := 0; i < 16; i++ {
+			if n.Children[i] != nil {
+				collapsed.Children[i], cached.Children[i], err = h.hash(n.Children[i], db, false)
+				if err != nil {
+					return original, original, err
+				}
+			} else {
+				collapsed.Children[i] = valueNode(nil) // Ensure that nil children are encoded as empty strings.
+			}
+		}
+		cached.Children[16] = n.Children[16]
+		if collapsed.Children[16] == nil {
+			collapsed.Children[16] = valueNode(nil)
+		}
+		return collapsed, cached, nil
+
+	default:
+		// Value and hash nodes don't have children so they're left as were
+		return n, original, nil
+	}
+}
+
+func (h *hasher) store(n node, db kvDatabase.IDriver, force bool) (node, error) {
+	// Don't store hashes or empty nodes.
+	if _, isHash := n.(hashNode); n == nil || isHash {
+		return n, nil
+	}
+	// Generate the RLP encoding of the node
+	h.tmp.Reset()
+	if err := rlp.Encode(h.tmp, n); err != nil {
+		panic("encode error: " + err.Error())
+	}
+
+	if h.tmp.Len() < 32 && !force {
+		return n, nil // Nodes smaller than 32 bytes are stored inside their parent
+	}
+	// Larger nodes are replaced by their hash and stored in the database.
+	hash, _ := n.cache()
+	if hash == nil {
+		h.sha.Reset()
+		h.sha.Write(h.tmp.Bytes())
+		hash = hashNode(h.sha.Sum(nil))
+	}
+	if db != nil {
+		return hash, db.Put(kvDatabase.KVItem{
+			Key:  hash,
+			Data: h.tmp.Bytes(),
+		})
+	}
+	return hash, nil
+}
