0f9c9c3710bba77b418008ebbd710a348e835c73
bianning <bianning18516822189@gmail.com>
2022-07-18 17:04:31 +0800

    feat: add account interface

---
+++ dataStructure/state/types.go
@@ -0,0 +1,27 @@
+package state
+
+import (
+	"github.com/SealSC/SealABC/common"
+	"math/big"
+)
+
+type Code []byte
+
+type Account interface {
+	Balance() *big.Int
+	SetBalance(*big.Int)
+
+	CodeHash() []byte
+	SetCodeHash([]byte)
+
+	Nonce() uint64
+	SetNonce(uint64)
+
+	Root() common.Hash
+	SetRoot(common.Hash)
+}
+
+type AccountTool interface {
+	NewAccount() Account
+	DecodeAccount([]byte) (Account, error)
+}
